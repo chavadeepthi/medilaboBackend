a0bac49d00a59ab1f5fe9de3fd00cd4532ea9ac6
chavadeepthi <ragadeepthi.chava@gmail.com>
2025-09-26 05:44:44 -0400

    Updated CORS settings

--- src/main/java/com/abernathy/medilabo/cors/CORSConfig.java
+++ src/main/java/com/abernathy/medilabo/cors/CORSConfig.java
@@ -1,20 +1,22 @@
 package com.abernathy.medilabo.cors;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CORSConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                // Apply CORS to all endpoints starting with /api/
-                registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:8080, \"http://localhost:8082") // your frontend URL
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:8080", "http://localhost:8082")
+                        .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                         .allowedHeaders("*")
+                        .exposedHeaders("*")
                         .allowCredentials(true);
             }
         };
